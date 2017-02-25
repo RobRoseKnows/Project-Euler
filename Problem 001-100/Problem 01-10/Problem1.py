e0373161fbf21191ff892daeccea3a2b39e6c2e2
Rob Rose <bertra97@gmail.com>
2017-02-25 17:45:16 -0500

    Added Problem2 prompt.

--- Problem 001-100/Problem 01-10/Problem1.py
+++ Problem 001-100/Problem 01-10/Problem1.py
@@ -1,14 +1,14 @@
 #!~/anaconda2/bin/python
 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
-# get 3, 5, 6 and 9. The sum of these multiples is 23.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #
 # Find the sum of all the multiples of 3 or 5 below N.
 #
 # Input Format
 # 
-# First line contains T that denotes the number of test cases. This is followed
-# by T lines, each containing an integer, N.
+# First line contains T that denotes the number of test cases. This is 
+# followed by T lines, each containing an integer, N.
 #
 # Constraints
 # 1 <= T <= 10^5
@@ -16,8 +16,8 @@
 #
 # Output Format
 #
-# For each test case, print an integer that denotes the sum of all the multiples
-# of 3 or 5 below N.
+# For each test case, print an integer that denotes the sum of all the 
+# multiples of 3 or 5 below N.
 
 import sys
 import math
